22ae2960af47d0167e5ff5521bad5538a31946ea
Gustavo Soares <gustavosoares.adm@gmail.com>
2020-07-11 18:28:05 -0300

    Inserindo projeto de esqueleto para testes web automatizados

---
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InformacoesUsuarioTest {
+    @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\gusta\\OneDrive\\Documentos\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+    }
+
+}
